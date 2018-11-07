c04cf8e5e5a6bcd7cc43b7704b687e7a2677479c
jonathan <jonathan.orillard@hotmail.fr>
2018-11-07 04:59:47 +0100

    Revert "Jo"

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import Accueil from './components/Accueil';
-import ContainerGraph from './components/ContainerGraph';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Accueil />
-        <ContainerGraph />
+        
       </div>
     );
   }
--- src/components/Accueil.jsx
+++ src/components/Accueil.jsx
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
-import './accueil.css';
 
 class Accueil extends Component {
     render() {
         return(
-            <header className='headerAccueil'>
-                <div className='h-100 w-100 position-relative d-flex flex-column align-items-center justify-content-center divHeaderAccueil'>
-                    <h1 className='display-4 text-white titleAccueil'>Predictiviz</h1>
-                    <span className='position-absolute text-white arrowScrollDown'><i className="fas fa-chevron-circle-down"></i></span>
-                </div>
-            </header>
+            <div>
+
+            </div>
         )
     }
 }
