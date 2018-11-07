d81422fa384013b21d59cb07d9cc799f3330b0b2
Jonathan Orillard <jonathan.orillard@hotmail.fr>
2018-11-07 04:35:40 +0100

    Menu

--- src/components/ContainerGraph.jsx
+++ src/components/ContainerGraph.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import './containerGraph.css';
+import { NavLink} from 'react-router-dom';
 
 class ContainerGraph extends Component {
     render() {
         return(
             <div>
-
+                <NavLink exact to="/"><span className='barre'> Plots </span></NavLink>
+                <NavLink to="/analysismethod"><span className='barre'> Analysis Method </span></NavLink>
+                <NavLink to="/correlationwbdata"><span className='barre'> Correlation WB data </span></NavLink>
+                <NavLink to="/causalityanalysis"><span className='barre'> Causality Analysis </span></NavLink>
+                <NavLink to="/predictionanalysis"><span className='barre'> Prediction Analysis </span></NavLink>
             </div>
         )
     }
